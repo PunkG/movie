2ab730c0fabab05982d451e18e4a3b6c2276c5e1
BrianZhang <zhang10.24@outlook.com>
2016-12-13 17:03:33 +0800

    test

--- src/MyTicketSale.java
+++ src/MyTicketSale.java
@@ -29,7 +29,7 @@ public void stop()
   {
     while(true)
     {
-      m_date=new Date();
+      m_date=new Date();//23
       String temp1=new String("日期:"+String.valueOf(1+m_date.getMonth())+"/"+String.valueOf(m_date.getDate())+"/"+String.valueOf(1900+m_date.getYear()));
       String temp2=new String("时间:"+String.valueOf(m_date.getHours())+":"+String.valueOf(m_date.getMinutes())+":"+String.valueOf(m_date.getSeconds()));
       gr.setText(temp1);
